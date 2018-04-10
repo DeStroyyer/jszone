99c5a093da2aa0272c767228fb9073ecabfd936c
Taras Voloshenko <taras.voloshenko@namecheap.com>
2018-04-10 16:40:24 +0300

    update fibonacci: array+error

--- fibbonacci/fib.js
+++ fibbonacci/fib.js
@@ -1,9 +1,37 @@
 function fib(number) {
-    return (number <= 1) ? 1 : fib(number - 1) + fib(number - 2);
+    try {
+        if (number > 21) {
+            throw new Error("Number is greater then 21")
+        }
+        else {
+            let array = [];
+            let first = 1;
+            let second = 2;
+            if (number >= 1) {
+                array.push(first);
+            }
+            if (number >= 2) {
+                array.push(second);
+            }
+            let temp;
+            for (let i = 3; i <= number; i++) {
+                temp = second;
+                second = temp + first;
+                first = temp;
+                array.push(second);
+            }
+            return array;
+        }
+    }
+    catch (err) {
+        console.log("error: " + err.message);
+    }
 }
+
 
 console.log(fib(1));
 console.log(fib(2));
 console.log(fib(3));
 console.log(fib(4));
 console.log(fib(5));
+console.log(fib(22));
