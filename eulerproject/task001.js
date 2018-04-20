dd24eeff09fed70c3da666bd4317d53dc92b1b78
Taras Voloshenko <taras.voloshenko@namecheap.com>
2018-04-20 19:59:46 +0300

    upd task #1

--- eulerproject/task001.js
+++ eulerproject/task001.js
@@ -1,9 +1,8 @@
 function count() {
-    return [...Array(999).keys()].map(x => ++x).filter(function (item) {
-        return item % 3 == 0 || item % 5 == 0
-    }).reduce((sum, current) => {
-        return sum + current;
-    }, 0)
+    return [...Array(999).keys()]
+        .map(x => ++x)
+        .filter((item) => {return item % 3 == 0 || item % 5 == 0})
+        .reduce((sum, current) => {return sum + current;}, 0)
 }
 
 console.log(count());
