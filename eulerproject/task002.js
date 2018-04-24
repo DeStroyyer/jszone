ebd6a5f4f4cabd972f8896a26319b39ade487ee5
Taras Voloshenko <taras.voloshenko@gmail.com>
2018-04-24 23:08:41 +0300

    task 2

--- eulerproject/task002.js
+++ eulerproject/task002.js
@@ -1,1 +1,17 @@
-let i =0;
+function count() {
+
+    function fib(arr) {
+        return arr[arr.length - 1] + arr[arr.length - 2];
+    }
+
+    function generator(array = [1, 2]) {
+        while (array[array.length - 1] < 4e+6) {array.push(fib(array));}
+            return array;
+    }
+
+    return console.log(generator()
+        .filter((x) => {return x % 2 == 0})
+        .reduce((sum, current) => {return sum + current;}, 0))
+};
+
+count();
