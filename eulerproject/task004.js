c2dda6f021b838cf1f42534e8af79521b722906a
Taras Voloshenko <taras.voloshenko@gmail.com>
2018-05-01 15:31:43 +0300

    task 4

--- eulerproject/task004.js
+++ eulerproject/task004.js
@@ -4,10 +4,14 @@ function reverseString(str) {
     return (str === '') ? '' : reverseString(str.substr(1)) + str.charAt(0);
 }
 
+function reverseString2(str) {
+    return String(str).split("").reverse().join("");
+}
+
 for (let i = 100; i < 1000; i++) {
     for (let j = 100; j < 1000; j++) {
         temp = i * j;
-        if ((reverseString(String(temp))) == temp) {
+        if ((reverseString2(String(temp))) == temp) {
             if (temp >= max) {
                 max = temp;
             }
