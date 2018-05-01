cb1775aaa62452ccf543bfaf3cdce4c9275c3452
Taras Voloshenko <taras.voloshenko@gmail.com>
2018-05-01 15:36:06 +0300

    task 4

--- eulerproject/task004.js
+++ eulerproject/task004.js
@@ -8,6 +8,14 @@ function reverseString2(str) {
     return String(str).split("").reverse().join("");
 }
 
+function reverseString3(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+
 for (let i = 100; i < 1000; i++) {
     for (let j = 100; j < 1000; j++) {
         temp = i * j;
