c464bbdfbd2265290393a28b11d81e6d6f425479
Taras Voloshenko <taras.voloshenko@namecheap.com>
2018-07-17 17:19:33 +0300

    test3

--- eulerproject/july2017/task03.js
+++ eulerproject/july2017/task03.js
@@ -1,3 +1,19 @@
-/**
- * Created by tvoloshenko on 7/17/18.
- */
+let max = 0;
+let data = 600851475143;
+function isPrime(n) {
+    for (let i = n - 1; i > 1; i--) {
+        if (n % i == 0) {
+            return false;
+        }
+        else continue;
+    }
+    return true;
+}
+
+for (let i = 1; i <= data; i++) {
+    if (isPrime(i) && data % i == 0 && max < +i) {
+        max = i;
+        data /= i;
+    }
+}
+console.log(max);
