0a26e12a28d2450e8a95c39946c75d729b14a27e
Taras Voloshenko <taras.voloshenko@namecheap.com>
2018-07-17 16:56:44 +0300

    test2_

--- eulerproject/july2017/task02.js
+++ eulerproject/july2017/task02.js
@@ -1,3 +1,19 @@
-/**
- * Created by tvoloshenko on 7/17/18.
- */
+// function fib(n) {
+//     if (n == 2) return 2;
+//     else return (n <= 1) ? n : fib(n - 1) + fib(n - 2);
+// }
+// console.log(fib(4));
+let total = 4000000;
+let a = 1;
+let b = 2;
+let temp;
+let sum = 0;
+while (b < total) {
+    if (b % 2 == 0) {
+        sum += b;
+    }
+    temp = b;
+    b = a + b;
+    a = temp;
+}
+console.log();
