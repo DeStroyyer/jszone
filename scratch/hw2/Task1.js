1a041a14a5e48f6c998708a92c5523a0df9fda30
Taras Voloshenko <taras.voloshenko@gmail.com>
2019-01-07 13:53:00 +0200

    1st task

--- scratch/hw2/Task1.js
+++ scratch/hw2/Task1.js
@@ -4,7 +4,27 @@
  * @returns {Array<number>} Returns array that contains positive numbers only.
  */
 function negativeFilter(input) {
-    return;
+    if (input.constructor.name != "Array") {
+        return;
+    } else {
+        let output = [];
+
+        function isPositive(num) {
+            if (num >= 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        for (let i = 0; i < input.length; i++) {
+            if (isPositive(input[i])) {
+                output.push(input[i]);
+            }
+        }
+        return output;
+    }
 }
 
 module.exports = negativeFilter;
